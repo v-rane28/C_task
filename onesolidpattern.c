13847d6609cf25b04f76db5ec118c1c74553c73d
v-rane28 <vaibhavrane2883@gmail.com>
2026-05-09 22:02:49 +0530

    seek

---
+++ onesolidpattern.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    for(int r=0;r<3;r++)
+    {
+        for(int c=0;c<5;c++)
+        {
+            printf("1");
+        }
+        printf("\n");
+    }
+    return 0;
+}
